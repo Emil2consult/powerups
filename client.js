39be0f8a1bdece63433c3da3463578fba8c695ef
Emil2consult <emil.s@aneheimconsulting.se>
2025-04-29 15:17:19 +0200

    Mmmm

--- client.js
+++ client.js
@@ -12,7 +12,8 @@ TrelloPowerUp.initialize({
               return t.alert({ message: 'No dates found.', duration: 4 });
             }
 
-            return t.alert({ message: 'Dates found.', duration: 10 });
+            const formatted = dates.map(d => d.toDateString()).join('\n');
+            return t.alert({ message: formatted, duration: 10 });
           });
       }
     }];
