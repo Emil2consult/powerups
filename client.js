f77b183152c766fc36c96b218250f1478b4ee57e
Emil2consult <emil.s@aneheimconsulting.se>
2025-04-29 14:17:01 +0200

    Test commit

--- client.js
+++ client.js
@@ -1,28 +1,18 @@
-var Promise = TrelloPowerUp.Promise;
-
 TrelloPowerUp.initialize({
-  'card-detail-badges': function(t, options) {
-    return t.card('desc')
-      .then(function(card) {
-        const description = card.desc || '';
-        const dates = findDates(description);
-
-        if (dates.length > 0) {
-          return [{
-            title: 'Found Date',
-            text: dates[0],
-            color: 'yellow',
-            refresh: 10
-          }];
-        } else {
-          return [];
+  'card-buttons': function (t, options) {
+    return [
+      {
+        text: "Hello world",
+        callback: function (t) {
+          return t.card().then(card => alert("Hello world"))
         }
-      });
+      }
+    ]
   }
 });
 
-function findDates(text) {
-  const dateRegex = /(\d{4}-\d{2}-\d{2})|(\d{2}\/\d{2}\/\d{4})/g;
-  const matches = text.match(dateRegex);
-  return matches || [];
-}
+//* function findDates(text) {
+//  const dateRegex = /(\d{4}-\d{2}-\d{2})|(\d{2}\/\d{2}\/\d{4})/g;
+//  const matches = text.match(dateRegex);
+//  return matches || [];
+//}
