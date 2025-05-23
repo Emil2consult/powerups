c0fa005521fba0f1a880e30bf7e33b8ff519c0da
Emil2consult <emil.s@aneheimconsulting.se>
2025-05-23 13:35:56 +0200

    popup test

--- rmDupes/rem_dups.js
+++ rmDupes/rem_dups.js
@@ -4,8 +4,18 @@ TrelloPowerUp.initialize({
       text: 'Remove duplicate',
       callback: async function(t) {
         await t.popup({
-          
-        })
+          title: 'Processing...',
+          url: 'processing.html',
+          height: 100
+        });
+
+        await new Promise(resolve => setTimeout(resolve, 10000));
+
+        return t.popup({
+          title: 'Done!',
+          url: 'done.html',
+          height: 150
+        });
       }
     }];
   }
