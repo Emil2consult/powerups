9e354eb3c7312a8c6a593c7b6fc82f322afa5f8b
Emil2consult <emil.s@aneheimconsulting.se>
2025-05-26 15:07:27 +0200

    test scan board for list

--- rmDupes/rem_dups.js
+++ rmDupes/rem_dups.js
@@ -1,12 +1,12 @@
 // On click function
 var onBtnClick = function (t, opts) {
   console.log('Someone clicked the button');
-  scanList(t);
+  scanBoardForList(t);
   t.alert({ message: 'Duplicates test.', duration: 4 });
 };
 
 // Send card to bin
-function scanList(t) {
+function scanBoardForList(t) {
   return t.board("all").then(function (board) {
       console.log(JSON.stringify(board, null, 2));
     });
