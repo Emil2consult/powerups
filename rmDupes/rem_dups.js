685ea8d6d3b0e4024f16e515d774ec4d332a0f0e
Emil2consult <emil.s@aneheimconsulting.se>
2025-05-26 15:07:01 +0200

    test scanBoardForList

--- rmDupes/rem_dups.js
+++ rmDupes/rem_dups.js
@@ -1,7 +1,16 @@
+// On click function
 var onBtnClick = function (t, opts) {
   console.log('Someone clicked the button');
+  scanList(t);
   t.alert({ message: 'Duplicates test.', duration: 4 });
 };
+
+// Send card to bin
+function scanList(t) {
+  return t.board("all").then(function (board) {
+      console.log(JSON.stringify(board, null, 2));
+    });
+}
 
 window.TrelloPowerUp.initialize({
   'board-buttons': function (t, opts) {
