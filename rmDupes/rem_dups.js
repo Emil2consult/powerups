e6548e7dae40f816bfc9175836dc48fc1cc91b8a
Emil2consult <emil.s@aneheimconsulting.se>
2025-05-26 09:15:47 +0200

    nytest

--- rmDupes/rem_dups.js
+++ rmDupes/rem_dups.js
@@ -1,32 +1,13 @@
-var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
-var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
-
 var onBtnClick = function (t, opts) {
   console.log('Someone clicked the button');
+  t.popup({message: "Hej", duration: 5})
 };
 
 window.TrelloPowerUp.initialize({
   'board-buttons': function (t, opts) {
     return [{
-      // we can either provide a button that has a callback function
-      icon: {
-        dark: WHITE_ICON,
-        light: BLACK_ICON
-      },
       text: 'Callback',
       callback: onBtnClick,
-      condition: 'edit'
-    }, {
-      // or we can also have a button that is just a simple url
-      // clicking it will open a new tab at the provided url
-      icon: {
-        dark: WHITE_ICON,
-        light: BLACK_ICON
-      },
-      text: 'URL',
-      condition: 'always',
-      url: 'https://trello.com/inspiration',
-      target: 'Inspiring Boards' // optional target for above url
     }];
   }
 });
