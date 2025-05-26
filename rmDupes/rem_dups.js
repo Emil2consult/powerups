80ed5cdd8bbaa9b8ec87859f03a10d6f99162797
Emil2consult <emil.s@aneheimconsulting.se>
2025-05-26 11:10:46 +0200

    button icon and alert

--- rmDupes/rem_dups.js
+++ rmDupes/rem_dups.js
@@ -1,12 +1,13 @@
 var onBtnClick = function (t, opts) {
   console.log('Someone clicked the button');
-  t.popup({message: "Hej", duration: 5})
+  t.alert({ message: 'Duplicates test.', duration: 4 });
 };
 
 window.TrelloPowerUp.initialize({
   'board-buttons': function (t, opts) {
     return [{
       text: 'Callback',
+      icon: './trash-x.png',
       callback: onBtnClick,
     }];
   }
